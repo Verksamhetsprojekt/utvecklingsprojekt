e29f6938878cc18206c7dbee741be75abb04d044
Jelena <jelmed16@student.hh.se>
2018-04-06 11:59:54 +0200

    request

--- utvecklingsprojekt/delete.php
+++ utvecklingsprojekt/delete.php
@@ -1,10 +1,10 @@
 <?php
 include('template.php');
-if(isset($_GET["artikelnr"]))
+if(isset($_REQUEST['artikelnr']))
 {
 	$query = <<<END 
 	DELETE FROM artikel
-	WHERE artikelnr = '{$_GET["artikelnr"]}'
+	WHERE artikelnr = '{$_REQUEST['artikelnr']}'
 END;
 $mysqli->query($query);
 header('Location:articles.php');
