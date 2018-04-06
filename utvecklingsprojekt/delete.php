0ef47351eaf69b3ea51bf2090ed17f377d53e26a
Jelena <jelmed16@student.hh.se>
2018-04-06 12:07:59 +0200

    ändrat tillbaka

--- utvecklingsprojekt/delete.php
+++ utvecklingsprojekt/delete.php
@@ -1,10 +1,10 @@
 <?php
 include('template.php');
-if(isset($_GET['id']))
+if(isset($_GET['artikelnr']))
 {
 	$query = <<<END 
 	DELETE FROM artikel
-	WHERE id = '{$_GET['id']}'
+	WHERE artikelnr = '{$_GET['artikelnr']}'
 END;
 $mysqli->query($query);
 header('Location:articles.php');
