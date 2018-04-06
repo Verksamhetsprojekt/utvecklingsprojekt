3f087f83490aa2135ae7895674dabfb637e97b6d
Jelena <jelmed16@student.hh.se>
2018-04-06 11:40:03 +0200

    tagit bort

--- utvecklingsprojekt/delete.php
+++ utvecklingsprojekt/delete.php
@@ -1,10 +1,10 @@
 <?php
 include('template.php');
-if(isset($_GET['artikelnr'])&&isset($_SESSION['userId']))
+if(isset($_GET['artikelnr']))
 {
 	$query = <<<END 
 	DELETE FROM artikel
-	WHERE id = '{$_GET['artikelnr']}'
+	WHERE artikelnr = '{$_GET['artikelnr']}'
 END;
 §mysqli->query($query);
 header('Location:articles.php');
