045ef0dcd8aa5c936b6240acb004795bb8c6eaef
Jelena <jelmed16@student.hh.se>
2018-04-06 11:14:13 +0200

    inget

--- utvecklingsprojekt/template.php
+++ utvecklingsprojekt/template.php
@@ -49,8 +49,8 @@ else
 	$navigation .= ' <a href="login.php">Logga in</a>';
 	$navigation .= ' <a href="register.php">Registrera dig</a>';
 }
-$navigation .= <<<END 
-</nav> 
+$navigation .= <<<END
+</nav>
 END;
 ?>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
