758764bcfa9c5c9a2365f15b9b85af5a4de1bba6
Jelena <jelmed16@student.hh.se>
2018-04-06 13:04:20 +0200

    edit article

---
+++ utvecklingsprojekt/edit_article.php
@@ -0,0 +1,40 @@
+<?php
+include('template.php');
+$content = ' ';
+if(isset($_GET['artikelnr']))
+{
+	if(isset($_POST['benamning']))
+	{
+		$query = <<<END
+		UPDATE artikel
+		SET benamning = '{$_POST['benamning']}',
+		utpris_prislista_a = '{$_POST['utpris_prislista_a']}',
+		lagerplats = '{$_POST['lagerplats']}'
+		WHERE artikelnr = '{$_GET['artikelnr']}'
+END;
+    $mysqli->query($query);
+	}
+	
+	$query = <<<END 
+	SELECT * FROM artikel
+	WHERE artikelnr = '{$_GET['artikelnr']}'
+END;
+
+$res = $mysqli->query($query);
+if($res->num_rows > 0)
+{
+	$row = $res->fetch_object();
+	$content = <<<END 
+	<form method="post" action="edit_article.php?artikelnr={$row->artikelnr}">
+    <input type="text" name="benamning" value="{$row->benamning}">
+    <input type="text" name="utpris_prislista_a" value="{$row->utpris_prislista_a}">
+    <input type="text" name="lagerplats" value="{$row->lagerplats}">
+    <input type="submit" Value="Spara">
+    </form>
+END;
+}
+}
+
+echo $navigation;
+echo $content;
+?>
