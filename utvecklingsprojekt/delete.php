3bbe2d693a0a4d7ba59918bc84b68470ff335040
Jelena <jelmed16@student.hh.se>
2018-04-06 11:33:53 +0200

    delete fil

---
+++ utvecklingsprojekt/delete.php
@@ -0,0 +1,13 @@
+<?php
+include('template.php');
+if(isset($_GET['artikelnr'])&&isset($_SESSION['userId']))
+{
+	$query = <<<END 
+	DELETE FROM artikel
+	WHERE artikelnr = '{$_GET['artikelnr']}'
+END;
+§mysqli->query($query);
+header('Location:articles.php');
+}
+echo $navigation;
+?>
