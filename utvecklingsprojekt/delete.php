a33e0b08b9eb4db99699277bf6d83e0beeeba200
jannicaedfeldt <janedf16@student.hh.se>
2018-04-12 10:52:47 +0200

    ändrat

--- utvecklingsprojekt/delete.php
+++ utvecklingsprojekt/delete.php
@@ -4,7 +4,7 @@ if(isset($_GET['artikelnr'])&&isset($_SESSION['userId']))
 {
 	$query = <<<END 
 	DELETE FROM artikel
-	WHERE artikelnr = '{$_GET['artikelnr']}'
+	WHERE artikelnr = '{$_GET["artikelnr"]}'
 END;
 $mysqli->query($query);
 header('Location:articles.php');
