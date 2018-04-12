e6a6c17516d3172acebd15d83adfc25547d2b561
jannicaedfeldt <janedf16@student.hh.se>
2018-04-12 14:59:38 +0200

    blö

--- utvecklingsprojekt/register.php
+++ utvecklingsprojekt/register.php
@@ -6,15 +6,12 @@ if(isset($_POST['username'])&&isset($_SESSION['userId']))
 	INSERT INTO users(username,password,email,fname,lname)
 	VALUES('{$_POST['username']}','{$_POST['password']}', '{$_POST['email']}',            
 	'{$_POST['fname']}', '{$_POST['lname']}')
+
 END;
 $mysqli->query($query);
-<<<<<<< HEAD
-header('Location:index.php');
-
-=======
 echo '<span style="color:Green">En ny användare har lagts till</span>';
 }
->>>>>>> f56872792e36a5fba7d9f5de21885ed8e66579a3
+
 $content = <<<END
 <form method="post" action="register.php">
 <input type="text" name="username" placeholder="Användarnamn">
@@ -25,11 +22,9 @@ $content = <<<END
 <input type="submit" Value="Registrera">
 </form>
 END;
-}
-else
-	{  
-		echo '<span style="color:Red">Du har inte behörighet. Vänligen logga in.</span>'; 
-	}
+
+
+
 echo $navigation;
 echo $content;
 echo $footer;
