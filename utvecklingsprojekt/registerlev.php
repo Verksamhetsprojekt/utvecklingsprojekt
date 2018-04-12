87f11493d3d9a3ceca4d4ff5b944b8e46a010b8d
Jelena <jelmed16@student.hh.se>
2018-04-12 13:05:16 +0200

    levname registrera

--- utvecklingsprojekt/registerlev.php
+++ utvecklingsprojekt/registerlev.php
@@ -1,10 +1,10 @@
 <?php
 include('template.php');
-if(isset($_POST['username']))
+if(isset($_POST['levname']))
 {
 	$query = <<<END
-	INSERT INTO userslev(username,password,email,name)
-	VALUES('{$_POST['username']}','{$_POST['password']}', '{$_POST['email']}',            
+	INSERT INTO userslev(levname,password,email,name)
+	VALUES('{$_POST['levname']}','{$_POST['password']}', '{$_POST['email']}',            
 	'{$_POST['name']}')
 END;
 $mysqli->query($query);
@@ -12,7 +12,7 @@ header('Location:index.php');
 }
 $content = <<<END
 <form method="post" action="register.php">
-<input type="text" name="username" placeholder="Användarnamn">
+<input type="text" name="levname" placeholder="Användarnamn">
 <input type="password" name="password" placeholder="Lösenord">
 <input type="text" name="email" placeholder="E-post">
 <input type="text" name="name" placeholder="Namn">>
