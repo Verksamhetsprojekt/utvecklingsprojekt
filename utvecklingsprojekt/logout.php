8f3dc87baeb3879aa5d823996db56146089ceb12
jannicaedfeldt <janedf16@student.hh.se>
2018-04-18 14:28:57 +0200

    ändring

--- utvecklingsprojekt/logout.php
+++ utvecklingsprojekt/logout.php
@@ -5,6 +5,12 @@ include('template.php');
 $_SESSION = array();
 session_destroy();
 
-header("Location:index.php");
+//header("Location:index.php");
+header("refresh:0;url=index.php");
+ echo '<script type="text/javascript">alert("Du loggas nu ut, välkommen åter");</script>';
+
+
 
 ?>
+
+
