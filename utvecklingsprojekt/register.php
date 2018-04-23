738f0a500501b9adf6c6fcd62763a838ba3816d6
jannicaedfeldt <janedf16@student.hh.se>
2018-04-23 15:47:29 +0200

    -->

--- utvecklingsprojekt/register.php
+++ utvecklingsprojekt/register.php
@@ -7,8 +7,12 @@
 	echo $navigation;
 	?>
 
+
+	
+
 <!-- Här börjar Body - här lägger du in kod som ska visas i $content -->
 	<body>
+
 		<div class="container text-center">
 			<?php
 			if(isset($_POST['username'])&&isset($_SESSION['userId']))
@@ -20,7 +24,7 @@
 
 
 END;
-
+$mysqli->query($query);
 echo '<script type="text/javascript">alert("Stämmer uppgifterna?");</script>';
 echo '<span style="color:Green">En ny användare har lagts till</span>';
 }
@@ -28,19 +32,20 @@ echo '<span style="color:Green">En ny användare har lagts till</span>';
  //<a href="delete.php?artikelnr={$row->artikelnr}="deletelink" onclick="return confirm('Are you sure?')">Radera artikel</a></br>
 
 $content = <<<END
+
 <form method="post" action="register.php">
 <input type="text" name="username" placeholder="Användarnamn">
 <input type="password" name="password" placeholder="Lösenord">
 <input type="text" name="email" placeholder="E-post">
 <input type="text" name="fname" placeholder="Förnamn">
 <input type="text" name="lname" placeholder="Efternamn">
-<input type="submit" Value="Registrera">
+<input type="submit" value="Registrera"></input>
 </form>
+
 END;
 
 if(!isset($_SESSION['userId'])) { //(!isset) betyder att det INTE är set
-	//header("Location:login.php");
-   //die("Du har inte behörighet. Vänligen logga in");
+
 	header("refresh:0;url=login.php");
  echo '<script type="text/javascript">alert("Du har inte behörighet, vänligen logga in");</script>';
 
